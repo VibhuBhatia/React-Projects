2724fab7584999d7b8fff151415cedeb08c46c4b
Vibhu Bhatia <vibhu@Vibhu-Bhatia.local>
2022-02-10 13:29:12 +0530

    first commit

---
+++ app/src/components/Expenses/ExpeneseItem.js
@@ -0,0 +1,29 @@
+import React,{ useState } from 'react';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+
+function ExpenseItem(props) {
+
+  const [title, settitle] = useState(props.title);
+
+
+  const clickhandler = () => {
+    console.log('button_clicked');
+    settitle("vibhu");
+  };
+
+  return (
+
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickhandler}>Change Title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
